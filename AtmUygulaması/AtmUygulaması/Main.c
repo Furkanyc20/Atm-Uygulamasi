3805960bb95734eeb4823ae6953154b4311463da
Furkanyc20 <furkanyucel2012hotmail.com>
2025-02-20 15:05:38 +0300

    Update Main.c

--- AtmUygulaması/AtmUygulaması/Main.c
+++ AtmUygulaması/AtmUygulaması/Main.c
@@ -3,7 +3,7 @@
 
 int main() {
 	int islemturu, Sorgu;
-	float anapara = 30000, Tutarbilgisi = 0, Çekilecekpara, Yatıralacakpara;
+	float anapara = 30000, Cekilecekpara, Yatiralacakpara;
 	printf("Yapmak istenilen islemi seciniz\n");
 	printf("Para Cekme - 1\nPara Yatirma - 2\nBakiye Bilgisi - 3\n");
 	printf("yapmak istediginiz islemin numarasini giriniz: ");
@@ -11,12 +11,12 @@ int main() {
 	if (islemturu == 1) {
 		printf("Hesaptaki Para: %.00f\n", anapara);
 		printf("ne kadar para cekmek isterseniz: ");
-		scanf_s("%f", &Çekilecekpara);
-		printf("Cekilecek Para:%.00f Emin misiniz?\nEvet - 1\nHayir -2\n", Çekilecekpara);
+		scanf_s("%f", &Cekilecekpara);
+		printf("Cekilecek Para:%.00f Emin misiniz?\nEvet - 1\nHayir -2\n", Cekilecekpara);
 		scanf_s("%d", &Sorgu);
 		if (Sorgu == 1) {
-			if (anapara >= Çekilecekpara) {
-				anapara = anapara - Çekilecekpara;
+			if (anapara >= Cekilecekpara) {
+				anapara = anapara - Cekilecekpara;
 				printf("kalan tutar: %.00f", anapara);
 			}
 			else {
@@ -33,33 +33,33 @@ int main() {
 	else if (islemturu == 2) {
 		printf("Hesaptaki Para: %.00f\n", anapara);
 		printf("Ne kadar para yatirmak isterseniz: ");
-		scanf_s("%f", &Yatıralacakpara);
-		if (Yatıralacakpara < 0) {
-			printf("Girilen Tutar Geçersizdir!");
+		scanf_s("%f", &Yatiralacakpara);
+		if (Yatiralacakpara < 0) {
+			printf("Girilen Tutar Gecersizdir!");
 		}
-		else if (Yatıralacakpara == 0) {
-			printf("Girilen tutar sıfırıdr islem iptal edildi.");
+		else if (Yatiralacakpara == 0) {
+			printf("Girilen tutar sifiridir islem iptal edildi.");
 		}
 		else {
-			printf("Girilen para: %.00f\nEmin misiniz?\nEvet - 1\nHayir - 2\n", Yatıralacakpara);
+			printf("Girilen para: %.00f\nEmin misiniz?\nEvet - 1\nHayir - 2\n", Yatiralacakpara);
 			scanf_s("%d", &Sorgu);
 			if (Sorgu == 1) {
-				anapara = Yatıralacakpara + anapara;
+				anapara = Yatiralacakpara + anapara;
 				printf("Paraniz yatirilmistir.\n Guncel Bakiye:%.00f", anapara);
 			}
 			else if (Sorgu == 2) {
-				anapara -= Yatıralacakpara;
+				anapara -= Yatiralacakpara;
 				printf("İsleminiz iptal edildi ");
 			}
 			else {
-				printf("Hatalı Numara Tekrar Giriniz.");
+				printf("Hatali Numara Tekrar Giriniz.");
 			}
 		}
 
 
 	}
 	else if (islemturu == 3) {
-		printf("Hesap Bakyesi = %.f", anapara);
+		printf("Hesap Bakyesi = %.00f", anapara);
 	}
 	else {
 		printf("Girilen sayi gecersizdir, Lutfen tekrar kontrol edin!");
